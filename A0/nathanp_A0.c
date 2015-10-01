60f6b9b824d825de5d7ee066b09f971e648faf07
admin <admin@Jamess-MacBook-Pro.local>
2015-09-30 22:16:28 -0700

    Directory for A0

---
+++ A0/nathanp_A0.c
@@ -0,0 +1,15 @@
+/* Hello World program */
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Nathan Pointer \n");
+
+    printf("I am primarily interested in Operating systems as a example of complex system design and as an opportunity to improve my ability to write code in c. \n");
+
+    printf("I just hope I have enough time to give the subject the attention it deserves. \n");
+
+    printf("I enjoy knitting. \n");
+
+}
